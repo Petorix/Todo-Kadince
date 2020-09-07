b4bd0a5be87864d14374ed4743c0844772f71b44
Petorix <pkwilliams1991@gmail.com>
2020-09-07 13:38:13 -0600

    --all

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const path = require("path");
 const routes = require("./routes/api");
 
 // NOT IMPLEMENTED - Local user authentication, to show that this is what I would use if it were to be implemented
-//      User model has been created for future use
-//      Task model has been adjusted to track user IDs
+//      * User model has been created for future use
+//      * Task model has been adjusted to track user IDs
 //
 // Docs:
 // passport config doc - http://www.passportjs.org/docs/configure/
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 8080;
 //  Username: kadinceUser
 //  Password: kadince123
 //  const MONGODB_URI = 'mongodb+srv://kadinceUser:kadince123@todo-kadince.jvj1g.mongodb.net/<dbname>?retryWrites=true&w=majority';
-mongoose.connect("mongodb://localhost/todo_app", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/todo_app", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -33,6 +33,12 @@ db.once("open", () => console.log("DB connected."));
 
 server.use(express.json());
 server.use(express.urlencoded({ encoded: false }));
+
+// For heroku connection
+if (process.env.NODE_ENV === "production") {
+  server.use(express.stat("client/build"));
+}
+
 server.use(morgan("tiny"));
 server.use("/", routes);
 server.listen(PORT, console.log(`Server is starting on port: ${PORT}`));
