3c5bb3bc625240f97d08c5be68284e0d2024c679
Petorix <pkwilliams1991@gmail.com>
2020-09-07 13:47:30 -0600

    upload to heroku

--- client/src/components/error_components/Error.js
+++ client/src/components/error_components/Error.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error(props) {
   return (
     <div>
-      <p>Error 404 - Page not found</p>
+      <p>Error 404 - Page not found - test</p>
     </div>
   );
 }
